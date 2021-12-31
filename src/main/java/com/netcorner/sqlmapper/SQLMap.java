c90cabd09e954df7886bd80b2acc84b4a0c12e56
netcorner <netcorner@qq.com>
2021-12-31 10:12:10 +0800

    错误的sql模板日志写入

--- src/main/java/com/netcorner/sqlmapper/SQLMap.java
+++ src/main/java/com/netcorner/sqlmapper/SQLMap.java
@@ -574,7 +574,8 @@ public class SQLMap   implements Serializable {
 //        		filterSqlString(qpage.getForm(), issearch);
 //        	}
 //        }
-		
+
+
         if(issearch){
 	        for(CRUDBase crud : statement.getSqlList()){
 	        	tmp=execStatementSQL(jdbc,crud,properties,qpage,statementid);
