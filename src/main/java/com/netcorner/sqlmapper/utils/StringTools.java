f3c5aa5704443d68a3e3156444ff5ecf0eb546b2
netcorner <sjf@2008>
2024-12-02 10:22:28 +0800

    sqlmapper中加入批量执行sql脚本的方法

--- src/main/java/com/netcorner/sqlmapper/utils/StringTools.java
+++ src/main/java/com/netcorner/sqlmapper/utils/StringTools.java
@@ -118,7 +118,7 @@ public  class StringTools {
         }
         else
         {
-            return String.format("%.2f bytes", filesize);
+            return String.format("%d bytes", filesize);
         }
     }
 	/**
